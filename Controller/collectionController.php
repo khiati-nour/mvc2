9254157af8d85be727a4a00beaa59a4ad2737a11
khiati-nour <72802257+khiati-nour@users.noreply.github.com>
2021-02-04 09:44:08 +0100

    Update collectionController.php

--- Controller/collectionController.php
+++ Controller/collectionController.php
@@ -4,37 +4,35 @@
 class collectionController
 {
 // TODO api: $books = new Books($_POST['search']);
-    public function render(array $GET, array $POST){
-if (isset($_POST['submit']))
-{
-    //TODO validate based on patterns?
-    $databaseManager = new DatabaseManager("localhost", 3306, "root","");
-    $databaseManager->connect();
+    public function render(array $GET, array $POST)
+    {
+        $databaseManager = new DatabaseManager("localhost", 3306, "root", "");
+        $databaseManager->connect();
+        $books = new Books($databaseManager);
+        $bookies = $books->getCollection();
+        if (isset($_POST['search'])) {
 
+            $searched_books = $books->find();
+        }
+        if (isset($_POST['submit'])) {
+            //TODO validate based on patterns?
 
+            if (!empty($_POST['isbn']) && !empty($_POST['author']) && !empty($_POST['genre']) && !empty($_POST['language']) && !empty($_POST['pages']) && !empty($_POST['published'])) {
+                $isbn = (int)$_POST['isbn'];
+                $title = $_POST['title'];
+                $author = $_POST['author'];
+                $genre = $_POST['genre'];
+                $language = $_POST['language'];
+                $pages = (int)$_POST['pages'];
+                $published = (int)$_POST['published'];
+                $id = 1;
 
-    $books = new Books($databaseManager);
+                $books->createBook($id, $isbn, $title, $author, $genre, $language, $pages, $published);
 
-    $bookies = $books->getCollection();
-    if (!empty($_POST['isbn']) && !empty($_POST['author']) && !empty($_POST['genre']) && !empty($_POST['language']) && !empty($_POST['pages']) && !empty($_POST['published']))
-{
-$isbn = (int)$_POST['isbn'];
-$title = $_POST['title'];
-$author = $_POST['author'];
-$genre = $_POST['genre'];
-$language = $_POST['language'];
-$pages = (int)$_POST['pages'];
-$published = (int)$_POST['published'];
-$id=1;
+            } else {
+                return $php_errormsg;
 
-$books->createBook( $id,$isbn, $title, $author, $genre, $language, $pages, $published);
-
-}
-
-else {
-    return $php_errormsg;
-
-}
-}
+            }
+        }
         require 'View/collection.php';
-}}
+    }}
