84c5526c570bd0539bfda52820028ed900886bf8
Manten Devillé <manten.d@hotmail.com>
2021-02-04 11:05:51 +0100

    Fix collapsable navigation, delete sign up & login from navbar

--- View/includes/nav.php
+++ View/includes/nav.php
@@ -1,36 +1,36 @@
 <nav class="navbar navbar-expand-md navbar-light navcolor sticky-top">
     <div class="container-fluid">
         <a href="#" class="navbar-brand logolink">
-            <img src="../assets/book.png" class="navlogo ">
             <p class="classlogoname">Bookie</p>
         </a>
 
 
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a href="#" class="nav-link">Bookieshelf</a>
+            <li class="nav-item">
+                    <a href="#" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Your Bookieshelf</a>
                 </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">User</a>
                 </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">Home</a>
-                </li>
+                
             </ul>
 
 
 
         </div>
 
-        <div class="userSettings collapse navbar-collapse container-fluid" id="userSettings">
+        <!-- <div class="userSettings collapse navbar-collapse container-fluid" id="userSettings">
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#" class="nav-link"><span class="fa fa-user"></span> Sign Up</a></li>
                 <li><a href="#" class="nav-link"><span class="fa fa-sign-in"></span> Login</a></li>
             </ul>
 
-        </div>
-        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#userSettings">
+        </div> -->
+        <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#navbarResponsive">
             <span class="navbar-toggler-icon"></span>
         </button>
     </div>
